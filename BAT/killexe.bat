22f787da4c47666f10aa7c2dd419afb7db6e167d
ssc-pc <1767883487@qq.com>
2017-05-30 12:46:22 +0800

    查找并终结进程

---
+++ BAT/killexe.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+Setlocal enabledelayedexpansion
+::CODER BY Administrator POWERD BY iBAT
+
+ECHO.
+tasklist|findstr "360WangPan.exe"
+
+if %errorlevel%==0 (taskkill /F /IM 360WangPan.exe) else (ECHO 该进程不存在或当前不在运行状态)
+
+pause
