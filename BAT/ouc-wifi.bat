dd0154639d328aa0abde705ea8426111e0305283
ssc-pc <1767883487@qq.com>
2017-05-30 13:04:25 +0800

    自动连接无线网络, 需使用配置文件*.xml

---
+++ BAT/ouc-wifi.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+Setlocal enabledelayedexpansion
+::CODER BY Administrator POWERD BY iBAT
+c:
+netsh wlan add profile filename="D:\Program Files\Batch\无线网络连接.xml" interface="无线网络连接" user=current
+netsh wlan connect name=OUC-WIFI ssid=OUC-WIFI
+echo %errorlevel%
+pause&exit
